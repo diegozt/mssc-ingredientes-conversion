bff93a5624eac9064e72b8dcd0e1d4eced8ef487
Diego <diego.zavala.t@gmail.com>
2021-04-26 22:03:26 -0500

    Adding Circuit Breaker - Resilence4j. Closes #2

--- src/main/java/com/dazt/msscingredientsconversion/controller/IngredientConversionController.java
+++ src/main/java/com/dazt/msscingredientsconversion/controller/IngredientConversionController.java
@@ -3,6 +3,8 @@ package com.dazt.msscingredientsconversion.controller;
 
 import com.dazt.msscingredientsconversion.dto.IngredientsConversionDTO;
 import com.dazt.msscingredientsconversion.proxy.IngredientConversionProxy;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 @RestController
+@Slf4j
 public class IngredientConversionController {
 
     @Autowired
@@ -40,17 +43,27 @@ public class IngredientConversionController {
                 ingredientsConversionDTO.getHostPort(), ingredientsConversionDTO.getLastUpdatedDate(), ingredientsConversionDTO.getCreationDate());
     }
 
+
     @GetMapping("/ingredient-conversion-feign/from/{from}/to/{to}/quantity/{quantity}/")
+    @Retry(name = "retry-times", fallbackMethod = "unavailableServiceError")
     public IngredientsConversionDTO calculateIngredientConversionFeign(
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity
     ) {
 
+        log.info("Invoking method: {}", "calculateIngredientConversionFeign");
+
         IngredientsConversionDTO ingredientsConversionDTO = proxy.retrieveMeasureValue(from, to);
 
         return new IngredientsConversionDTO(ingredientsConversionDTO.getFrom(), ingredientsConversionDTO.getTo(), quantity,
                 ingredientsConversionDTO.getConversionFactor(), ingredientsConversionDTO.getConversionFactor().multiply(quantity),
                 ingredientsConversionDTO.getHostPort(), ingredientsConversionDTO.getLastUpdatedDate(), ingredientsConversionDTO.getCreationDate());
+    }
+
+    public IngredientsConversionDTO unavailableServiceError(Exception ex) {
+        IngredientsConversionDTO dto = new IngredientsConversionDTO();
+        dto.setFrom("error");
+        return dto;
     }
 }
